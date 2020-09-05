2da5340675605a04fecaed5b5b865edd58b17527
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-09-05 20:40:18 +0300

    Update README, implement cvToTensor

--- README.md
+++ README.md
@@ -4,4 +4,5 @@
 2. Download cuDNN from the NVIDIA site [link](https://developer.nvidia.com/rdp/cudnn-download), registration needed
 3. Create dir with name `external` in the project root.
 4. Put cuDNN and libtorch in it.
-5. Install openCV (openSUSE -> `sudo zypper in opencv opencv-devel`)
+5. Install openCV (openSUSE -> `sudo zypper in opencv opencv-devel`)  
+Other option to install all needed stuff is to use `https://github.com/microsoft/vcpkg` and delete lines 8-10 from CmakeList.txt
--- src/mnist.cpp
+++ src/mnist.cpp
@@ -1,15 +1,37 @@
 #include "mnist.h"
+#include <opencv2/opencv.hpp>
+
+torch::Tensor CvImageToTensor(const cv::Mat &image) {
+    assert(image.channels() == 1);
+    std::vector<int64_t> dims{
+            static_cast<int64_t>(1),
+            static_cast<int64_t>(image.rows),
+            static_cast<int64_t>(image.cols)
+    };
+    torch::Tensor tensor_image = torch::from_blob(
+            image.data,
+            torch::IntArrayRef(dims),
+            torch::TensorOptions().dtype(torch::kFloat).requires_grad(false)).clone();
+    // clone is required to copy data from temporary object
+
+    return tensor_image.to(torch::DeviceType::CUDA);
+}
 
 MNISTDataset::MNISTDataset(const std::string &images_file_name, const std::string &labels_file_name) {
 
 }
 
 torch::data::Example<> MNISTDataset::get(size_t index) {
-    return torch::data::Example<>();
+    return {
+            CvImageToTensor(images_[index]), torch::tensor(static_cast<int64_t>(labels_[index]),
+                                                           torch::TensorOptions().dtype(torch::kLong).device(
+                                                                   torch::DeviceType::CUDA))
+    };
 }
 
+// number of labels loaded from disk
 torch::optional<size_t> MNISTDataset::size() const {
-    return torch::optional<size_t>();
+    return labels_.size();
 }
 
 void MNISTDataset::ReadLabels(const std::string &labels_file_name) {
@@ -19,3 +41,5 @@ void MNISTDataset::ReadLabels(const std::string &labels_file_name) {
 void MNISTDataset::ReadImages(const std::string &images_file_name) {
 
 }
+
+
